b10cb5def362fbfb3a24eceb9e898f138fbd66dd
xipovie <adzaouri@gmail.com>
2023-07-19 16:55:01 -0700

    h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,31 @@
 #include "main.h"
 
 /**
- * print_sign - Determines if the input number is greater, equal, or less than zero.
+ * print_sign - Determines if the input number is greater,
+ * equal, or less than zero.
  * @n: The input number as an integer.
  *
- * Return: 1 if the number is greater than zero, 0 if it is zero, and -1 if it is less than zero.
+ * Return: 1 if the number is greater than zero,
+ * 0 if it is zero, and -1 if it is less than zero.
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar(43); /* '+' */
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar(48); /* '0' */
-        return (0);
-    }
-    else
-    {
-        _putchar(45); /* '-' */
-        return (-1);
-    }
+	if (n > 0)
+	{
+	_putchar(43); /* '+' */
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar(48); /* '0' */
+	return (0);
+	}
+	else
+	{
+	_putchar(45); /* '-' */
+	return (-1);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,13 +8,12 @@
  */
 int _abs(int i)
 {
-    if (i >= 0)
-    {
-        return (i);
-    }
-    else
-    {
-        return (-i);
-    }
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
 }
-

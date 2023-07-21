4983c2272eb212381f8a7bfdbface988b43237f1
xipovie <adzaouri@gmail.com>
2023-07-21 14:56:55 -0700

    h

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,19 +7,17 @@
  */
 void print_most_numbers(void)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; x <= 9; x++)
+	for (; x <= 9; x++)
 	{
-		if (x == 2 || x == 4)
-			continue;
-		_putchar(x + '0');
+	if (x == 2 || x == 4)
+	{continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
 	}
 	_putchar('\n');
-}
-
-int main(void)
-{
-	print_most_numbers();
-	return (0);
 }

91310f9ad19cdd9c59d25182662e0fccd2ca63ab
xipovie <adzaouri@gmail.com>
2023-07-25 13:21:26 -0700

    j

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,14 +20,14 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			putchar(str[i]);
+			_putchar(str[i]);
 		}
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			putchar(str[n + 1]);
+			_putchar(str[n + 1]);
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
